681e3b7d0c8f322af7e5f8408ec696fc38463695
MarianPussetto <marianpussetto@gmail.com>
2023-10-03 20:40:12 -0300

    actualizo calculadora

---
+++ src/calculator/Calculadora.java
@@ -0,0 +1,18 @@
+package calculator;
+
+public class Calculadora {
+
+    int num1;
+    int num2;
+
+    public void sumar(int num1, int num2) {
+
+        System.out.println("La suma de los números es " + (num1 + num2));
+    }
+
+    public void restar(int num1, int num2) {
+
+        System.out.println("La resta de los números es " + (num1 - num2));
+    }
+
+}
---
+++ src/calculator/Calculator.java
@@ -0,0 +1,15 @@
+
+
+package calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        
+        Calculadora c = new Calculadora();
+        
+       c.sumar(10,12);
+       c.restar(12, 10);
+    }
+
+}
